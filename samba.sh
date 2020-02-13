110b6ad6517b48425dcec63575dbfeab513aba4e
Loose Coupling <loosecpl@gmail.com>
2020-02-13 17:48:03 +0900

    Create a user using mitamae

--- samba.sh
+++ samba.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
-# Create a smb.conf
+# Setup samba
 (cd /root && ./mitamae local setup.rb)
-
-groupadd -g $SMB_USER_GID $SMB_USER
-useradd -u $SMB_USER_UID -g $SMB_USER_GID $SMB_USER
-cat <<PASSWORD | passwd $SMB_USER
-$SMB_PASSWORD
-$SMB_PASSWORD
-PASSWORD
-cat <<PASSWORD | smbpasswd -a -s $SMB_USER
-$SMB_PASSWORD
-$SMB_PASSWORD
-PASSWORD
 
 exec /usr/sbin/smbd -F -S --no-process-group
