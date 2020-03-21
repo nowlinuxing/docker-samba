e9237e79cd0fcfc76a9394f23a4fb86bcede6777
Loose Coupling <loosecpl@gmail.com>
2020-03-21 18:41:21 +0900

    Can specify a json file instead of arguments

--- up.sh
+++ up.sh
@@ -1,38 +1,46 @@
 #!/bin/sh
 
-if [ $# -lt 4 ]; then
-  echo "Usage: up.sh user uid gid share_dir"
-  exit 1
-fi
+read_password()
+{
+  echo -n "Enter a password: "
+  stty -echo
+  read -r smb_password
+  stty echo
+}
 
-user=$1
-uid=$2
-gid=$3
-share_dir=$4
+case $# in
+  1)
+    smb_param=$(cat $1)
+    read_password
+    smb_param=$(echo "$smb_param" | jq --arg password "$smb_password" '.user.password = $password')
 
-echo -n "Enter a password: "
-stty -echo
-read -r smb_password
-stty echo
-echo
+    share_dir=$(echo $smb_param | jq .share.path)
+    ;;
+  4)
+    user=$1
+    uid=$2
+    gid=$3
+    share_dir=$4
 
-echo "user: $user"
-echo "uid: $uid"
-echo "gid: $gid"
-echo "share_dir: $share_dir"
+    read_password
 
-smb_password_json=$(echo "$smb_password" | sed -e 's|\\|\\\\|g' -e 's|"|\\"|g')
-smb_param=$(cat <<PARAM
+    smb_param=$(cat <<PARAM | jq --arg password "$smb_password" '.user.password = $password'
 {
   "user": {
     "name": "$user",
-    "password": "$smb_password_json",
     "uid": $uid,
     "gid": $gid
   }
 }
 PARAM
-)
+    )
+    ;;
+  *)
+    echo "Usage: up.sh user uid gid share_dir"
+    echo "       up.sh param.json"
+    exit 1
+    ;;
+esac
 
 sudo docker run \
   --rm \
