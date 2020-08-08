bad876e63fc71eb3e1dd5621c933d0a7acc6cc0f
Loose Coupling <loosecpl@gmail.com>
2020-08-08 17:44:51 +0900

    Support multi users

--- mitamae/setup.rb
+++ mitamae/setup.rb
@@ -1,26 +1,27 @@
 param = JSON.parse(ENV["SMB_PARAM"])
-user = param["user"]
 
 template "/etc/samba/smb.conf" do
   variables(param: param)
 end
 
 # Create a user and a samba user
-group user["name"] do
-  gid user["gid"]
-end
+param["users"].each do |user|
+  group user["name"] do
+    gid user["gid"]
+  end
 
-user user["name"] do
-  uid user["uid"]
-  gid user["gid"]
-  password user["password"]
-end
+  user user["name"] do
+    uid user["uid"]
+    gid user["gid"]
+    password user["password"]
+  end
 
-execute "Add a samba user" do
-  command <<-COMMAND.gsub(/^ */, "")
-    cat <<'PASSWORD' | smbpasswd -a -s #{user["name"]}
-    #{user["password"]}
-    #{user["password"]}
-    PASSWORD
-  COMMAND
+  execute "Add a samba user" do
+    command <<-COMMAND.gsub(/^ */, "")
+      cat <<'PASSWORD' | smbpasswd -a -s #{user["name"]}
+      #{user["password"]}
+      #{user["password"]}
+      PASSWORD
+    COMMAND
+  end
 end
